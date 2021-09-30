ff693b9b787b7efc808f58c3ebb943b4ef7ed3db
Swapnil Kondiba Dhanawate <84777131+iamdsk@users.noreply.github.com>
2021-09-30 18:25:48 +0530

    Add files via upload

---
+++ Tourism-Web-App-FrontEnd-master/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './HomeComponent/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />    
+      <header className="App-header">  
+      </header>
+    </div>
+  );
+}
+export default App;
+   
